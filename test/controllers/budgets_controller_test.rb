b5761de50c6016a64c691c8930caeaa5b0dabfdf
Pier Puccini <pierpuccini@users.noreply.github.com>
2026-03-27 23:18:59 -0500

    Add budget statement-cycle toggle

---
+++ test/controllers/budgets_controller_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class BudgetsControllerTest < ActionDispatch::IntegrationTest
+  include EntriesTestHelper
+
+  setup do
+    sign_in @user = users(:family_admin)
+    @family = @user.family
+  end
+
+  test "show renders statement-cycle toggle and preserves it on today link" do
+    get budget_path(Budget.date_to_param(Date.new(2026, 3, 1)), use_statement_cycles: "1")
+
+    assert_response :ok
+    assert_select "a", text: "Statement cycles: On"
+    assert_select "a[href*='use_statement_cycles=1']", text: "Today"
+    assert_select "span", text: "Statement cycles"
+  end
+end
